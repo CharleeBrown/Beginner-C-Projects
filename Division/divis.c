8e9a57b38582789dd6d25da9957a61ec63777e33
Lee <orusmusic01@gmail.com>
2023-06-23 14:40:07 -0500

    Division section added

---
+++ Division/divis.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+	long int divOne;
+	long int divTwo;
+	double result;
+
+	printf("Enter the first number \n");
+	scanf("%ld", &divOne);
+	printf("Enter the dviding number \n");
+	scanf("%ld", &divTwo);
+	result = (double)divOne/(double)divTwo;
+	printf("Results : %.3f \n", result);
+}
