fe81d0f0b907dd5518934167cff1d928893959c8
Lee <orusmusic01@gmail.com>
2023-06-28 15:59:40 -0500

    salary breakdown script

---
+++ salarysee/getsal.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int calc(int hours);
+int main(){
+	long double yearlySalary;
+	long double hourlyRate;
+	const float taxAmount = 0.735;
+	const double workHours = 40;
+	int res;
+
+
+	switch(answer){
+		case 1:
+			try{
+				test here
+				}
+			catch (Exception ex){
+				ex.Message();
+			}
+		case 2:
+			try{
+				test here}
+			catch (Exception ex){
+				ex.Message()}
+	}
+
+
+
+	printf("Enter your work Hours");
+
+	res = calc(workHours);
+
+	printf("%d",res);
+
+		
+}
+
+int calc(int hours){
+	int res = hours * 2;
+	return res;
+}
+
+
+
