1828bbba7bc7b132b9a95ac15f94c3c88e8ed1eb
Lee <orusmusic01@gmail.com>
2023-06-29 15:43:09 -0500

    Added formatting to string len script

--- LenStr/lenstring.c
+++ LenStr/lenstring.c
@@ -2,10 +2,10 @@
 #include <string.h>
 
 int main(){
-	char test[30];
+	char test[400];
 	printf("Enter a string\n ");
 	scanf("%s",test);
 	int len = strlen(test);
-	printf("The word %s", test);
-	printf(" is %d characters long \n", len);
+	printf("The word \n\n %s \n\n", test);
+	printf("is %d characters long \n", len);
 }
